69e868fe5ae7132872bb157972c9a09d8a7c7a3a
NHammond <natehammond62@gmail.com>
2023-09-30 17:16:40 -0600

    initial commit

---
+++ landing_page/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="landing-page">
+      
+      <div className="background-image">
+      </div>
+
+      <div className="content">
+        <h1 className="title">AIION X</h1>
+        <p className="teaser">Ride without limits</p>
+        <button className="btn">Reserve Now</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
