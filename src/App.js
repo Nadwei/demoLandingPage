b3c7db22f1887a97dbb57a4484853c0a30e82dfc
NHammond <natehammond62@gmail.com>
2023-10-08 12:03:02 -0600

    styling updates

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ function App() {
   return (
     <div className="landing-page">
       
-      <div className="background-image">
-      </div>
-
       <div className="content">
         <h1 className="title">AIION X</h1>
         <p className="teaser">Ride without limits</p>
         <button className="btn">Reserve Now</button>
       </div>
+      
     </div>
   );
 }
